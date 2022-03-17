8ea85f168d2373ce5af750b40f1e5e42f7d448c3
Ilya <72158359+al1enjesus@users.noreply.github.com>
2022-03-17 10:53:45 +0000

    Add array initialisation

--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
 /* hello.c */
 #include <stdio.h>
+#include <stdlib.h>
 
 void main (void)
 {
+
+  int n = 10;
+  int* arr = (int*) malloc(sizeof(int) * n);
+  
+  
 	printf ("Hello World\n");
 	printf ("Goodbye World\n");
 }
