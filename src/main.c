5b270f420171956d6e2f8a6f1f892db1d5570692
Ilya <72158359+al1enjesus@users.noreply.github.com>
2022-03-17 13:54:33 +0000

    Add digits_sum, max functions.

--- src/main.c
+++ src/main.c
@@ -2,12 +2,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main (void)
+
+int digits_sum(int element) {
+  char str[80];
+  sprintf(str, "%d", element);
+
+  int sum_digits = 0;
+  for (int i = 0; str[i] != '\0'; i++){
+    sum_digits += str[i] - '0';
+  }
+    
+  return sum_digits;
+}
+
+int max(int num1, int num2)
+{
+    return (num1 > num2 ) ? num1 : num2;
+}
+
+int main ()
 {
 
   int n = 10;
   int* arr = (int*) malloc(sizeof(int) * n);
+
+  printf("ARRAY:\n[");
+  for (int i = 0; i < n; i++) {
+    arr[i] = rand();
+    printf("%d", arr[i]);
+    if (i != n - 1) printf(", ");
+    else printf("]\n");
+  }
   
+  int max_digits_sum=-1;
+  for (int i = 0; i < n; i++) {
+    max_digits_sum = max(max_digits_sum, digits_sum(arr[i]));
+  }
+
+  printf("Max digits sum: %d\n", max_digits_sum);
   
 	printf ("Hello World\n");
 	printf ("Goodbye World\n");
